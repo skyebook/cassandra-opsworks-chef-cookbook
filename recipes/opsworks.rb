23cf4405d5d659e0946227468c3df1db40bddf30
Skye Book <skye.book@gmail.com>
2013-08-29 14:37:32 -0400

    Add opsworks recipe and attributes

---
+++ recipes/opsworks.rb
@@ -0,0 +1,22 @@
+include_recipe "cassandra::datastax"
+
+seed_array = []
+node[:opsworks][:layers][:cassandra][:instances].each{|instance| seed_array << instance.private_ip}
+
+
+
+if seed_array.empty?
+  seed_array << node[:ipaddress]
+end
+  
+default[:cassandra][:seeds] => seed_array
+
+%w(cassandra.yaml cassandra-env.sh).each do |f|
+  template File.join(node["cassandra"]["conf_dir"], f) do
+    source "#{f}.erb"
+    owner node["cassandra"]["user"]
+    group node["cassandra"]["user"]
+    mode  0644
+    notifies :restart, resources(:service => "cassandra")
+  end
+end
