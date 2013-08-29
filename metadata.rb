9bf991051033e7c37bf967303fe199d5ad04ba0a
Skye Book <skye.book@gmail.com>
2013-08-29 15:54:57 -0400

    Remove tarball installer and java dependency

--- metadata.rb
+++ metadata.rb
@@ -6,5 +6,4 @@ description      "Installs/Configures OpsCode Apache Cassandra"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.0.0"
 
-depends "java"
 depends "apt"
